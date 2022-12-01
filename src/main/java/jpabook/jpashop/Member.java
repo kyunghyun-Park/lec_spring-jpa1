338d1607e25ee2ac7467fa4c973bbed44eaefe29
kyunghyun-park <qqqq_o@naver.com>
2022-12-01 16:43:14 +0900

    221201 test error 잡았음

--- src/main/java/jpabook/jpashop/Member.java
+++ src/main/java/jpabook/jpashop/Member.java
@@ -1,10 +1,11 @@
 package jpabook.jpashop;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 @Getter @Setter
