1a9f6c22d3c6c8a24f55a0e0326c41153fe2db0b
kyunghyun-park <qqqq_o@naver.com>
2022-12-09 14:21:48 +0900

    221209 entity2

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.CascadeType.ALL;
 import static javax.persistence.FetchType.*;
 
 @Entity
@@ -25,11 +26,12 @@ public class Order {
 
     //@OneToMany : 한 개의 주문은 여러개의 주문아이템을 가질 수 있음
     //@mappedBy : 연관관계의 주인이 아닌 쪽 [연관관계의 주인인 OrderItem 클래스의 order(필드명) 기재]
-    @OneToMany(mappedBy = "order")
+    //cascade :
+    @OneToMany(mappedBy = "order" , cascade = ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     //하나의 주문은 하나의 배송
-    @OneToOne(fetch = LAZY)
+    @OneToOne(fetch = LAZY, cascade = ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -39,4 +41,28 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
+    //==연관관계 편의메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+    //==연관관계 편의 메서드==//
+
+    /*public static void main(String[] args) {
+        Member member =  new Member();
+        Order order = new Order();
+
+        //member.getOrders().add(order); //이 라인이 없어도 됨
+        order.setMember(member);
+    }*/
 }
