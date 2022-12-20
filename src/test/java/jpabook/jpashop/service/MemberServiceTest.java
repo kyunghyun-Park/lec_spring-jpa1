57b02db5d29a6a8921200c64784aaa9aff2a9428
kyunghyun-park <qqqq_o@naver.com>
2022-12-20 11:47:20 +0900

    221219 commit

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,37 +6,49 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringRunner.class) //스프링과 테스트 통합
+@SpringBootTest             //스프링 부트 띄우고 테스트(이게 없으면 @Autowired 다 실패)
 @Transactional
 public class MemberServiceTest {
 
-    @Autowired MemberService memberService ;
+    @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
 
     @Test
+    @Rollback(false)
     public void 회원가입() throws Exception {
         //given
         Member member = new Member();
         member.setName("park");
 
         //when
+        Long savedId = memberService.join(member);
 
         //then
+        assertEquals(member, memberRepository.findOne(savedId));
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
         //given
+        Member member1 =  new Member();
+        member1.setName("park");
+
+        Member member2 =  new Member();
+        member2.setName("park");
 
         //when
+        memberService.join(member1);
+        memberService.join(member2);   //예외가 발생해야 한다! (중복이름)
 
         //then
-
+        fail("예외가 발생해야 한다.");
     }
 }
